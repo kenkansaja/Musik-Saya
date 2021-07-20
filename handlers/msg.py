f7eda499fd2269882081959bc0fdf3585d21790e
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-20 08:52:45 +0700

    Create msg.py

---
+++ handlers/msg.py
@@ -0,0 +1,15 @@
+class Messages():
+      HELP_MSG = [
+"""
+**🔰 Perintah**
+      
+• /play (nama lagu) - Untuk Memutar lagu yang Anda minta melalui youtube
+• /song [nama lagu]: Unduh audio lagu dari youtube
+• /skip: Melewati trek saat ini
+• /pause: Jeda trek
+• /resume: Melanjutkan trek yang dijeda
+• /end: Menghentikan pemutaran media
+      
+Semua Perintah Bisa Digunakan Kecuali Perintah /player /skip /pause /resume  /end Hanya Untuk Admin Grup
+"""
+]
