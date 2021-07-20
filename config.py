e1900648eef68f0acb00bab1f1ac3ad58ba23c34
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-20 11:52:32 +0700

    Create config.py

--- config.py
+++ config.py
@@ -16,26 +16,6 @@ OWNER = getenv("OWNER")
 GROUP = getenv("GROUP")
 CHANNEL = getenv("CHANNEL")
 OWNER = getenv("OWNER")
-UPDATES_SUB = getenv("UPDATES_SUB")
 
 
-class Config(object):
-  
-  SESSION_NAME = getenv("SESSION_NAME", "session")
-  BOT_TOKEN = getenv("BOT_TOKEN")
-  BOT_NAME = getenv("BOT_NAME")
 
-  API_ID = int(getenv("API_ID"))
-  API_HASH = getenv("API_HASH")
-
-  DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
-
-  COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-
-  SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-  ASSISTANT = getenv("ASSISTANT")
-  OWNER = getenv("OWNER")
-  GROUP = getenv("GROUP")
-  CHANNEL = getenv("CHANNEL")
-  OWNER = getenv("OWNER")
-  UPDATES_SUB = getenv("UPDATES_SUB")
