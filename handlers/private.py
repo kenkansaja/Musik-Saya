3fb7fa07a7101cbceb61e2c2e57791d4911171ed
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-20 11:17:18 +0700

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -19,7 +19,7 @@ async def start(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "📚 PANDUAN", text=f"{HELP_MSG}")
+                        "📚 PANDUAN", callback_data = f"help+1")
                   ],[
                     InlineKeyboardButton(
                         "💬 Group Support", url=f"https://t.me/{GROUP}"
