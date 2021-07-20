789cf74b4da0abbcff6a41e24fee0ee6bedf7e09
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-20 20:41:26 +0700

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -11,7 +11,7 @@ def time_to_seconds(time):
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
 
-@Client.on_message(filters.command('song') & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command('so') & ~filters.private & ~filters.channel)
 def song(client, message):
 
     user_id = message.from_user.id 
