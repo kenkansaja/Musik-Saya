acd639386a9abb0b27cf90581e3c444f6dfee5e8
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-20 08:32:57 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,25 +3,20 @@ from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv()
-
-  SESSION_NAME = getenv("SESSION_NAME", "session")
-  BOT_TOKEN = getenv("BOT_TOKEN")
-  BOT_NAME = getenv("BOT_NAME")
-
-  API_ID = int(getenv("API_ID"))
-  API_HASH = getenv("API_HASH")
-
-  DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
-
-  COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-
-  SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-  ASSISTANT = getenv("ASSISTANT")
-  OWNER = getenv("OWNER")
-  GROUP = getenv("GROUP")
-  CHANNEL = getenv("CHANNEL")
-  OWNER = getenv("OWNER")
-  UPDATES_SUB = getenv("UPDATES_SUB")
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+ASSISTANT = getenv("ASSISTANT")
+OWNER = getenv("OWNER")
+GROUP = getenv("GROUP")
+CHANNEL = getenv("CHANNEL")
+OWNER = getenv("OWNER")
+UPDATES_SUB = getenv("UPDATES_SUB")
 
 
 class Config(object):
