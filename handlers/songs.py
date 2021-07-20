3c453fa6b14e4f276b8ac530aaf57ca8ae3559c5
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-20 21:33:40 +0700

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -45,7 +45,7 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("🔁 mengunduh lagu...")
+    m.edit("🔁 **Mengunduh lagu...**")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
