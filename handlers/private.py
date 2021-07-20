ed88f48e25e7cc8bade53cccd3e1a49471b9a92e
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-20 09:36:36 +0700

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -7,7 +7,7 @@ from helpers.filters import other_filters2
 
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
-    await message.reply_sticker("CAACAgUAAxkBAAEKORJguwR4VsN1PCqbNh82LgABstqGU2EAAjMCAAK8hthX2HwTiIOxnxofBA")
+    await message.reply_sticker("aHR0cHM6Ly90Lm1lL2FkZHN0aWNrZXJzL1N0aWNrZXJCeUtlbmthbmFzd18xX2FuaW0=")
     await message.reply_text(
         f"""hai 👋, saya dapat memutar lagu di voice chat group anda.
 
