76d88f1e92b26f9ca70f44a9376238e0e12142a1
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-20 07:52:49 +0700

    Add files via upload

---
+++ handlers/private.py
@@ -0,0 +1,78 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn, CHANNEL, GROUP, ASSISTANT, OWNER
+from helpers.filters import other_filters2
+
+
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+    await message.reply_sticker("CAACAgUAAxkBAAEKORJguwR4VsN1PCqbNh82LgABstqGU2EAAjMCAAK8hthX2HwTiIOxnxofBA")
+    await message.reply_text(
+        f"""hai 👋, saya dapat memutar lagu di voice chat group anda.
+
+➠ Tekan tombol panduan menggunakan bot di bawah jika ingin mengetahui bagaimana cara menggunakan saya.
+
+➠ Tambahkan juga  @{ASSISTANT} ke dalam grup jika anda ingin menambahkan saya ke grup anda.
+        """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "📚 PANDUAN", callback_data="help")
+                  ],[
+                    InlineKeyboardButton(
+                        "💬 Group Support", url="https://t.me/{GROUP}"
+                    ),
+                    InlineKeyboardButton(
+                        "🔊 Channel Support", url="https://t.me/{CHANNEL}"
+                    )
+                ],[ 
+                    InlineKeyboardButton(
+                        "🎁 Kirim Donasi", url="https://t.me/{OWNER}"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("start") & other_filters)
+async def gstart(_, message: Message):
+      await message.reply_text("""**✅ ava music player is online**""",
+      reply_markup=InlineKeyboardMarkup(
+                  [
+                      [
+                          InlineKeyboardButton(
+                              "💬 GROUP", url=f"https://t.me/{GROUP}"
+                          ),
+                          InlineKeyboardButton(
+                              "OWNER 👮", url=f"https://t.me/{OWNER}"
+                          )
+                      ]
+                  ]
+              )
+         )
+         
+@Client.on_message(filters.command("help") & other_filters)
+async def ghelp(_, message: Message):
+    await message.reply_text(
+      f"""
+**🔰 Perintah**
+      
+• /play (nama lagu) - Untuk Memutar lagu yang Anda minta melalui youtube
+• /song [nama lagu]: Unduh audio lagu dari youtube
+• /skip: Melewati trek saat ini
+• /pause: Jeda trek
+• /resume: Melanjutkan trek yang dijeda
+• /end: Menghentikan pemutaran media
+      
+Semua Perintah Bisa Digunakan Kecuali Perintah /player /skip /pause /resume  /end Hanya Untuk Admin Grup
+""",
+      reply_markup=InlineKeyboardMarkup(
+                  [
+                    [InlineKeyboardButton(text = '🔵 ᴏᴡɴᴇʀ', url = f"t.me/{OWNER}")],
+                    [InlineKeyboardButton(text = '👥 ɢʀᴏᴜᴘ', url=f"https://t.me/{SUPPORT_GROUP}"),
+                     InlineKeyboardButton(text = 'ᴄʜᴀɴɴᴇʟ 📣', url=f"https://t.me/{UPDATES_CHANNEL}")]
+                ]
+        )
+    )        
---
+++ handlers/ytsearch.py
@@ -0,0 +1,40 @@
+# the logging things
+import logging
+
+from pyrogram.types import Message
+from search_engine_parser import GoogleSearch
+from youtube_search import YoutubeSearch
+
+from pyrogram import Client as app, filters
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+import pyrogram
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+@app.on_message(pyrogram.filters.command(["search"]))
+async def ytsearch(_, message: Message):
+    try:
+        if len(message.command) < 2:
+            await message.reply_text("/search (judul lagu)!")
+            return
+        query = message.text.split(None, 1)[1]
+        m = await message.reply_text("Searching....")
+        results = YoutubeSearch(query, max_results=5).to_dict()
+        i = 0
+        text = ""
+        while i < 5:
+            text += f"Judul - {results[i]['title']}\n"
+            text += f"Durasi - {results[i]['duration']}\n"
+            text += f"Penonton - {results[i]['views']}\n"
+            text += f"Channel - {results[i]['channel']}\n\n"
+            text += f"`https://youtube.com{results[i]['url_suffix']}`\n"
+            text += f"──────────────────────\n\n"
+            i += 1
+        await m.edit(text, disable_web_page_preview=True)
+    except Exception as e:
+        await message.reply_text(str(e))
