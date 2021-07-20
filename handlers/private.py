3659b112b56ac577b689e9ae85e0a150abce2901
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-20 20:42:04 +0700

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -52,7 +52,7 @@ async def gstart(_, message: Message):
               )
          )
 
-@Client.on_message(filters.command("help") & ~filters.private & ~filters.channel)
+@Client.on_message(filters.command("hp") & ~filters.private & ~filters.channel)
 async def ghelp(_, message: Message):
     await message.reply_text(
       f"""
