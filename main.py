6519532a968a10014c97b86089dc3af12d868682
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-21 14:59:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from pyrogram import Client as Bot
 
 from callsmusic import run
-from config import API_ID, API_HASH, BOT_TOKEN, BG_IMAGE
-
-response = requests.get(BG_IMAGE)
-file = open("./etc/foreground.png", "wb")
-file.write(response.content)
-file.close()
+from config import API_ID, API_HASH, BOT_TOKEN
 
 bot = Bot(
     ":memory:",
