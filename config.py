363afeed830149b8335a6e5e0d3a287e51165bf7
KEN KAN <84455320+kenkansaja@users.noreply.github.com>
2021-07-25 00:03:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,31 +4,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 SESSION_NAME = getenv("SESSION_NAME", "session")
-
 BOT_TOKEN = getenv("BOT_TOKEN")
-
 BOT_NAME = getenv("BOT_NAME")
-
 API_ID = int(getenv("API_ID"))
-
 API_HASH = getenv("API_HASH")
-
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
-
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-
 ASSISTANT = getenv("ASSISTANT")
-
 OWNER = getenv("OWNER")
-
 GROUP = getenv("GROUP", "musikkugroup")
-
 CHANNEL = getenv("CHANNEL", "musikkuchannel")
-
 OWNER = getenv("OWNER", "kenkanasw")
-
 PANDUAN = getenv("PANDUAN", "https://telegra.ph/Panduan-07-20")
-
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/7dfd80f33fca97cc14b75.png")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
